d2b60922ceb11e003f1d3552f03607ce57c67256
Nazar Klypych <xxxnazarkoxxx@gmail.com>
2022-10-08 10:23:19 +0300

    configs

--- config.py
+++ config.py
@@ -1,10 +1,13 @@
+import os
+
+
 class Config:
     DEBUG = True
-    SECRET_KEY = 'cnjwn3hnkjjn39jd'
-    PG_USER = 'postgres'
-    PG_PSSWRD = 'nazar2415'
-    PG_HOST = 'localhost'
-    PG_PORT = '5050'
-    PG_DATABASE = 'sparkchat'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    PG_USER = os.environ.get('PG_USER')
+    PG_PSSWRD = os.environ.get('PG_PSSWRD')
+    PG_HOST = os.environ.get('PG_HOST')
+    PG_PORT = os.environ.get('PG_PORT')
+    PG_DATABASE = os.environ.get('PG_DATABASE')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = f"postgresql+psycopg2://{PG_USER}:{PG_PSSWRD}@{PG_HOST}/{PG_DATABASE}"
